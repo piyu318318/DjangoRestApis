ef345bfd6d5af6e86e90e940932f0ab4d4a0f463
piyush dixit <dixitpiyushk@gmail.com>
2024-12-01 19:07:13 +0530

    added folder name and laibraries in settings.py in INSTALLED_APPS

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bmvefw)1*tq6m!^fp#@nx##3m5@jw)&s*@rm6rk1z!)ol)7wag
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'myproject'
 ]
 
 MIDDLEWARE = [
