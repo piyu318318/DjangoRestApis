43c4f6bbbda633f25dd6f42c937310ccc6a680a6
piyush dixit <dixitpiyushk@gmail.com>
2024-12-01 19:11:43 +0530

    create the user model in models.py

--- myproject/myproject_apis/models.py
+++ myproject/myproject_apis/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    userId = models.AutoField(primary_key=True)
+    userName = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.userName
